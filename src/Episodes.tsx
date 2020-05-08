165352918fa486a74ff64dd60c25e66315e2ed06
Danny Navarro <j@dannynavarro.net>
2020-05-08 16:54:17 +0100

    List episode titles

--- src/Episodes.tsx
+++ src/Episodes.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 
-import { Toolbar } from "@material-ui/core";
+import { Toolbar, ListItem, ListItemText, List } from "@material-ui/core";
 
 import TopBar from "./TopBar";
+
+interface EpisodeItemProp {
+  title: string;
+}
+
+const EpisodeItem = (props: EpisodeItemProp) => {
+  return (
+    <ListItem>
+      <ListItemText primary={props.title} />
+    </ListItem>
+  );
+};
 
 interface EpisodesProps {
   drawerHandler: () => void;
@@ -10,12 +22,26 @@ interface EpisodesProps {
 }
 
 export default (props: EpisodesProps) => {
+  const [episodes, setEpisodes] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://127.0.0.1:3030/episodes")
+      .then((response) => response.json())
+      .then((data) => {
+        setEpisodes(data);
+      });
+  }, []);
+
   return (
     <React.Fragment>
       <TopBar drawerHandler={props.drawerHandler} title="Episodes" />
       <main>
         <Toolbar />
-        <p>Hola</p>
+        <List>
+          {episodes.map((item: any) => (
+            <EpisodeItem title={item.title} />
+          ))}
+        </List>
       </main>
     </React.Fragment>
   );
