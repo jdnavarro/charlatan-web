4b4999e577905eff6ca58eb1c3427d750fb47696
Danny Navarro <j@dannynavarro.net>
2020-07-11 13:23:26 +0100

    Better error handling for podcast API calls

--- src/podcast/api.ts
+++ src/podcast/api.ts
@@ -11,30 +11,32 @@ export const list = (token: string): Promise<Podcasts> =>
       headers: { Authorization: `Bearer ${token}` },
     })
     .then(({ data }) => data)
-    .catch((err) => handleError({}, err));
+    .catch((err) => {
+      handleError(err);
+      return {};
+    });
 
-export const add = (token: string, url: string): Promise<void> =>
+export const add = (token: string, url: string) =>
   axios({
     method: "post",
     url: `${API_URL}/podcasts`,
     data: { url },
     headers: { Authorization: `Bearer ${token}` },
-  }).catch((err) => handleError(undefined, err));
+  }).catch(handleError);
 
-export const remove = (token: string, id: string): Promise<void> =>
+export const remove = (token: string, id: string) =>
   axios
     .delete(`${API_URL}/podcasts/${id}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
-    .catch((err) => handleError(undefined, err));
+    .catch(handleError);
 
-const handleError = (r: any, err: any) => {
+const handleError = (err: any) => {
   if (err.response.data.type === "Unconfigured") {
     navigate("/register");
   } else if (err.response.data.type === "UnverifiedToken") {
     navigate("/login");
   } else {
     console.error(err.message);
-    return r;
   }
 };
